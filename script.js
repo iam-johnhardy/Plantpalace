3e44e10e925585fb9ac62058d2e8c0d96e9a57b0
John Hardy <unatahjohn@gmail.com>
2025-06-20 17:14:32 +0100

    sec commit

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 function showSidebar() {
         const sidebar = document.querySelector('.sidebar')
         sidebar.style.display = 'flex'
-      }
+      };
 // hide the sidebar
 function hideSidebar() {
         const sidebar = document.querySelector('.sidebar')
         sidebar.style.display = 'none'
-      }
+      };
 
     //  search functionality
 function search() {
@@ -21,5 +21,68 @@ function search() {
           } else {
             item.style.display = 'none'
           }
-        })
-      } 
+        });
+      };
+
+
+      // Change Picture Hero Section(indoor)
+
+      const imageSources = ['indoor plants.webp','img 5.jpg','img 6.jpg'];
+
+      let currentIndex = 0;
+      const imageElement = document.getElementById('rotating-image');
+
+      function changeImage() {
+        currentIndex = (++currentIndex) % imageSources.length
+
+
+        imageElement.style.opacity = 0;
+        
+        setTimeout(() => {
+        imageElement.src = imageSources[currentIndex];
+          imageElement.style.opacity = 1;
+        }, 300);
+      }
+
+      changeImage();
+
+      const intervalid = setInterval(changeImage, 5000);
+
+
+
+
+
+
+      // API functionality
+      const API_KEY = sk-jyPt68317da456bdd10615;
+      const API_URL =  `https://perenual.com/api/v2/species-list?key=sk-jyPt68317da456bdd10615&page=1`;
+
+      async function fetchPlant() {
+        try{
+          const response = await fetch(API_URL);
+          const data = await response.json();
+          displayPlants(data);
+        } catch (error){
+          console.error("Error Fetching Plant:", error);
+        };
+        
+      };
+
+
+      function displayPlants(plants)
+      {
+        const container = document.getElementById("plant-container");
+        plants.forEach(plant =>{
+          const card = document.createElement("div");
+          card.className = "plant-card";
+          card.innerHTML = `<h2>${plant.common_name}  </h2>
+          <img src="${plant.default_image?.thumbnail || "no-image.jpg"}" alt="${plant.common_name}">
+
+          <p><strong>Watering:</strong> ${plant.watering}</p>
+          <p><strong>sunlight:</strong> ${plant.sunlight?.joint(", ")} </p>
+          
+          `;
+        });
+      };
+
+      fetchPlant();
