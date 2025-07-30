84ba767ec8a9c327e95c3217aa6455c4c1dfc52e
John Hardy <unatahjohn@gmail.com>
2025-07-30 16:10:54 +0100

    third commit

--- script.js
+++ script.js
@@ -11,20 +11,7 @@ function disableSidebar() {
         disableSidebar.style.display = 'none'
       };
 
-    //  search functionality
-function search() {
-        const searchInput = document.querySelector('.search-input')
-        const searchValue = searchInput.value.toLowerCase()
-        const items = document.querySelectorAll('.item')
-        items.forEach(item => {
-          const itemName = item.querySelector('h3').textContent.toLowerCase()
-          if (itemName.includes(searchValue)) {
-            item.style.display = 'block'
-          } else {
-            item.style.display = 'none'
-          }
-        });
-      };
+
 
 
       // Change Picture Hero Section(indoor)
